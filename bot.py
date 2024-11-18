99092de0b4beff2284864a7f2f845c190730c266
Ahmed Zakaria <ahmed.zakria@halan.com>
2024-11-18 15:25:26 +0200

    some changes

--- bot.py
+++ bot.py
@@ -149,7 +149,7 @@ def start_bot():
         app.add_handler(CommandHandler('start', welcome_user))
         app.add_handler(CallbackQueryHandler(button_handler))
         app.add_handler(CallbackQueryHandler(ask_more_handler, pattern="yes_more|no_more"))
-        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, add_note_handler))
+        # app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, add_note_handler))
         app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, save_note_handler))
 
         print("Bot is running...")
